4afb01db7e431575e9488cf30b10e7ba0365de4f
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-05 13:59:29 +0530

    updated one

--- social-media/src/App.js
+++ social-media/src/App.js
@@ -1,11 +1,8 @@
-
+import React from "react";
+import Home from "./pages/home/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
-  );
+  return <Home/>
 }
 
 export default App;
