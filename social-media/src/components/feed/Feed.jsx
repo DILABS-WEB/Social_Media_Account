a49f32dad4096fcd249c7c89506f40ece261b1d4
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-06 12:51:42 +0530

    updated topbar

---
+++ social-media/src/components/feed/Feed.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './feed.css'
+
+export default function Feed() {
+  return (
+    <div className='share'>
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <div className="shareBottom">
+            
+          </div>
+        </div>
+      </div>
+      
+    </div>
+
+  )
+}
---
+++ social-media/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,130 @@
+import React from 'react'
+import "./sidebar.css"
+import RssFeed from '@mui/icons-material/RssFeed';
+import Chats from '@mui/icons-material/Chat';
+import Video from '@mui/icons-material/VideoChat';
+import Groups from '@mui/icons-material/Groups';
+import Book from '@mui/icons-material/Bookmark';
+import Question from '@mui/icons-material/QuestionMark';
+import Jobs from '@mui/icons-material/Work';
+import Event from '@mui/icons-material/Event';
+import School from '@mui/icons-material/School';
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListitem">
+            <RssFeed className='sidebarIcon'/>
+            <span className="sidebarListItemText">
+              Feed
+            </span>
+          </li>
+          <li className="sidebarListitem">
+            <Chats className='sidebarIcon'/>
+            <span className="sidebarListItemText">
+              Chats
+            </span>
+          </li>
+          <li className="sidebarListitem">
+            <Video className='sidebarIcon'/>
+            <span className="sidebarListItemText">
+              Videos
+            </span>
+          </li>
+          <li className="sidebarListitem">
+            <Groups className='sidebarIcon'/>
+            <span className="sidebarListItemText">
+              Groups
+            </span>
+          </li>
+          <li className="sidebarListitem">
+            <Book className='sidebarIcon'/>
+            <span className="sidebarListItemText">
+              Bookmarks
+            </span>
+          </li>
+          <li className="sidebarListitem">
+            <Question className='sidebarIcon'/>
+            <span className="sidebarListItemText">
+              Quesions
+            </span>
+          </li>
+          <li className="sidebarListitem">
+            <Jobs className='sidebarIcon'/>
+            <span className="sidebarListItemText">
+              Jobs
+            </span>
+          </li>
+          <li className="sidebarListitem">
+            <Event className='sidebarIcon'/>
+            <span className="sidebarListItemText">
+              Events
+            </span>
+          </li>
+          <li className="sidebarListitem">
+            <School className='sidebarIcon'/>
+            <span className="sidebarListItemText">
+              Courses
+            </span>
+          </li>
+        </ul>
+      <button className='sidebarButton'>
+        Show More
+      </button>
+      <hr className='sidebarHr'/>
+      <ul className="sidebarFriendList">
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+        <li className="sidebarFriend">
+          <img src="/assets/person/2.jpg" alt="" className="sidebarFriendImg" />
+          <span className="sidebarFriendName">Abhishek Kumar</span>
+        </li>
+      </ul>
+      </div>
+    </div>
+  )
+}
