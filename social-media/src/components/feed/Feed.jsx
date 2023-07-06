28e250dcd3832416b01bfb3716299c24a474b1a3
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-06 16:41:35 +0530

    updated feed

--- social-media/src/components/feed/Feed.jsx
+++ social-media/src/components/feed/Feed.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import './feed.css'
 import Share from '../share/Share'
 
+
 export default function Feed() {
   return (
     <div className='feed'>
       <div className="feedWrapper">
       <Share/>
+  
+      
+      
       </div>
       
     </div>
