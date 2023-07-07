c13ab8b9b5e22e33989cd00f87f16970b3a71cfe
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-07 15:25:28 +0530

    feed and dummy data

--- social-media/src/components/post/Post.jsx
+++ social-media/src/components/post/Post.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import "./post.css"
 import { MoreVert } from '@mui/icons-material'
 
-export default function Post() {
-  return (
+export default function Post(post) {
+ console.log(post);
+    return (
     <div className='post'>
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">  
                     <img className='postProfileImg' src="/assets/person/22.jpg" alt="" srcset="" /> 
                     <span className="postUsername">Aditya Krishnan Nambiar</span>
-                    <span className="postDate">5 mins ago</span>
+                    <span className="postDate">{}</span>
                  </div>
                
                 <div className="postTopRight"> <MoreVert/>  </div>
@@ -18,17 +19,17 @@ export default function Post() {
             </div>
             <div className="postCenter">
                 
-                <span className="postText">Hey its my first post:) </span>
+                <span className="postText">{} </span>
                 <img className='postImg' src="../assets/post/1.jpeg" alt="" srcset="" />
             </div>
             <div className="postBottom">
                <div className="postBottomLeft">
                 <img className='likeIcon' src="assets/Like.png" alt="" />
                 <img className='likeIcon'  src="assets/heart.png" alt="" />
-                <span className='postLikeCounter'>32 people like it</span>
+                <span className='postLikeCounter'>{} people like it</span>
                </div>
                <div className="postBottomRight">
-                <span className="postCommentText">9 comments</span>
+                <span className="postCommentText">{} comments</span>
                </div>
             </div>
         </div>
