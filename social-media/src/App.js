5fc1de41d77b12b2e1b129916201801afc0af5b5
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-10 15:00:59 +0530

    Added p-rofile js page into it

--- social-media/src/App.js
+++ social-media/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Home from "./pages/home/Home";
 import Person from '@mui/icons-material/Person';
+import Profile from "./pages/home/profile/Profile";
 
 function App() {
-  return <Home/>
+  return <>
+  {/* <Home/>  */}
+  <Profile/></>
 }
 
 export default App;
