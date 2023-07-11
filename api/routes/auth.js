f649cd0af9fd60990ca79f083624a3def2f26e0a
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-07-11 14:51:02 +0530

    updated with api

---
+++ api/routes/auth.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+import User from "../models/User";
+import { genSalt, hash, compare } from "bcrypt";
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    //generate new password
+    const salt = await genSalt(10);
+    const hashedPassword = await hash(req.body.password, salt);
+
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("wrong password")
+
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+export default router;
